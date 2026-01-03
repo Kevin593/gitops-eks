dae29869ecf1dd225b0017d6aa00c0d78125e89e
Kevin Caiza <kevincaiza131@gmail.com>
2026-01-02 22:48:31 -0500

    deply app

---
+++ apps/src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const VERSION = "1.0.0";
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.get("/version", (req, res) => {
+  res.json({ version: VERSION });
+});
+
+app.listen(3000, () => {
+  console.log("App running on port 3000");
+});
