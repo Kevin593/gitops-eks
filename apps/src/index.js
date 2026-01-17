9821dcd551fd34fb6532738ae2482d76213919d8
Kevin Caiza <kevincaiza131@gmail.com>
2026-01-17 11:06:39 -0500

    metricas js

--- apps/src/index.js
+++ apps/src/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
-const app = express();
+const client = require("prom-client");
 
-const VERSION = "1.0.29";
+const app = express();
+const VERSION = "1.0.30";
+
+/* ============================
+   Prometheus config
+============================ */
+
+// Registro de métricas
+const register = client.register;
+
+// Métricas por defecto (CPU, memoria, etc.)
+client.collectDefaultMetrics();
+
+// Endpoint para Prometheus
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
+});
+
+/* ============================
+   Endpoints de la app
+============================ */
 
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });
