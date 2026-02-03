f3803bcb8e119ec405e1e94a95ee784e07a7ac06
Kevin Caiza <kevincaiza131@gmail.com>
2026-02-03 13:55:31 -0500

    mejora grafna

--- apps/src/index.js
+++ apps/src/index.js
@@ -1,40 +1,28 @@
 const express = require("express");
-const client = require("prom-client"); // librería para métricas Prometheus
+const client = require("prom-client");
 
 const app = express();
-const VERSION = "1.0.5";
+const VERSION = "1.0.1";
 
 /* ============================
    Prometheus metrics
 ============================ */
 
-// Registro de métricas
+// Registro por defecto
 const register = client.register;
 
-// Métrica de contador para peticiones
+// Contador de peticiones HTTP
 const requestCounter = new client.Counter({
   name: "api_requests_total",
   help: "Total de peticiones a la API",
   labelNames: ["endpoint"]
 });
 
-// Métrica de histograma para tiempo de respuesta individual
-const responseHistogram = new client.Histogram({
-  name: "api_response_time_seconds",
-  help: "Tiempo de respuesta de cada solicitud en segundos",
-  labelNames: ["endpoint"],
-  buckets: [0.001, 0.01, 0.1, 0.5, 1, 2, 5, 10] // rangos para medir cada solicitud
-});
-
 /* ============================
-   Middleware para métricas
+   Middleware de métricas
 ============================ */
 app.use((req, res, next) => {
-  const end = responseHistogram.startTimer({ endpoint: req.path });
-  requestCounter.inc({ endpoint: req.path }); // contar petición
-  res.on("finish", () => {
-    end(); // registrar tiempo de respuesta al terminar
-  });
+  requestCounter.inc({ endpoint: req.path });
   next();
 });
 
@@ -45,7 +33,7 @@ app.get("/version", (req, res) => {
   res.json({ version: VERSION });
 });
 
-// Endpoint para Prometheus
+// Endpoint de métricas
 app.get("/metrics", async (req, res) => {
   res.set("Content-Type", register.contentType);
   res.end(await register.metrics());
