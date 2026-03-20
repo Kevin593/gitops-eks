add9cd3573d1f7b961c67b04f7650ab6de1db35a
Kevin Caiza <kevincaiza131@gmail.com>
2026-03-19 23:33:29 -0500

    probar subida9k

--- apps/src/index.js
+++ apps/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const client = require("prom-client");
 
 const app = express();
-const VERSION = "1.0.1008";
+const VERSION = "1.0.18";
 
 /* ============================
    Prometheus metrics
