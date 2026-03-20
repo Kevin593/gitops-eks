6f5056192a7c5638b442d54486868cf7b5139f2b
Kevin Caiza <kevincaiza131@gmail.com>
2026-03-20 17:04:07 -0500

    feat: agregar tekton trigger pipeline

--- apps/src/index.js
+++ apps/src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const client = require("prom-client");
 
 const app = express();
-const VERSION = "1.0.18798";
+const VERSION = "1.0.188798";
 
 /* ============================
    Prometheus metrics
